e83fcbd0248d6e0b5a27c40dea4f5e3062050e06
Joaquin Tolentino <joacotol@yahoo.ca>
2016-12-03 15:05:23 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+  $("#Works").click(function() {
+    $(".b_eh").css("display", "none");
+    $(".v_eh").css("display", "none");
+    $(".l_eh").css("display", "none");
+    $(".h_eh").css("display", "none");
+    $(".wiki_eh").css("display", "none");
+    $(".w_eh").css("display", "inline-block");
+  });
+
+  $("#Bio").click(function() {
+    $(".w_eh").css("display", "none");
+    $(".v_eh").css("display", "none");
+    $(".l_eh").css("display", "none");
+    $(".h_eh").css("display", "none");
+    $(".wiki_eh").css("display", "none");
+    $(".b_eh").css("display", "inline-block");
+  });
+
+  $("#CV").click(function() {
+    $(".w_eh").css("display", "none");
+    $(".b_eh").css("display", "none");
+    $(".l_eh").css("display", "none");
+    $(".h_eh").css("display", "none");
+    $(".wiki_eh").css("display", "none");
+    $(".v_eh").css("display", "inline-block");
+  });
+
+  $("#LP").click(function() {
+    $(".w_eh").css("display", "none");
+    $(".b_eh").css("display", "none");
+    $(".v_eh").css("display", "none");
+    $(".h_eh").css("display", "none");
+    $(".wiki_eh").css("display", "none");
+    $(".l_eh").css("display", "inline-block");
+  });
+
+  $("#Hist").click(function() {
+    $(".w_eh").css("display", "none");
+    $(".b_eh").css("display", "none");
+    $(".v_eh").css("display", "none");
+    $(".l_eh").css("display", "none");
+    $(".wiki_eh").css("display", "none");
+    $(".h_eh").css("display", "inline-block");
+  });
+
+  $("#Wikh").click(function() {
+    $(".w_eh").css("display", "none");
+    $(".b_eh").css("display", "none");
+    $(".v_eh").css("display", "none");
+    $(".l_eh").css("display", "none");
+    $(".h_eh").css("display", "none");
+    $(".wiki_eh").css("display", "inline-block");
+  });
+});
